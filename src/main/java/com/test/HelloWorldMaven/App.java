2672321b13377a666b038fd05d53f43614a886de
mokshkher <61618015+mokshkher@users.noreply.github.com>
2020-03-14 13:50:05 +0530

    Update App.java

--- src/main/java/com/test/HelloWorldMaven/App.java
+++ src/main/java/com/test/HelloWorldMaven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, My First Java Program" );
+        System.out.println( "Hello World, My First Java Program, testing Jenkins" );
     }
 }
